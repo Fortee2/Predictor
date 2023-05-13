7740b748043ea1b7f3d7a620bb029beb4dd9c916
Randy Costner <costner.randy@gmail.com>
2023-05-13 09:57:00 -0400

    Random Search
    Implemented Random Search for Parameter fitting

--- main.py
+++ main.py
@@ -5,36 +5,52 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 from collections import defaultdict
 import gym
-from gym import spaces
-
+from gym import spaces    
+import random
 class StockTradingEnvironment(gym.Env):
-    def __init__(self, data, max_holding_period=30):
+    def __init__(self, data, max_holding_period=120):
         super(StockTradingEnvironment, self).__init__()
         self.data = data
         self.max_holding_period = max_holding_period
         self.current_step = 0
         self.current_holding_period = 0
         self.in_position = False
+        self.capital = 10000
 
         self.action_space = spaces.Discrete(3)  # Buy, Sell, Hold
         #When adding new data points, make sure to update the shape of the observation space
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(18,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(11,), dtype=np.float32)
 
     def reset(self):
         self.current_step = 0
         self.current_holding_period = 0
+        self.capital = 10000 
         self.in_position = False
         return self.get_state()
 
     def get_state(self):
         #when adding new data points, make sure to update the state
-        return self.data.iloc[self.current_step][[ 'open','close', 'volume', 'updown', 'high', 'low','MACD', 'Signal', 'rsi', 'market_open','market_close', 'market_volume', 'market_updown', 'market_high', 'market_low','market_MACD', 'market_Signal', 'market_rsi']].values
+        return self.data.iloc[self.current_step][[ 'open','close', 'volume', 'high', 'low','MACD','Signal', 'rsi', 'market_updown']].values
+
+    def take_action(self, action):
+        if action == 0:  # Buy
+            if not self.in_position:
+                self.in_position = True
+                self.current_holding_period = 0
+                self.capital -= self.data.iloc[self.current_step]['close']
+        elif action == 1:  # Sell
+            if self.in_position:
+                self.in_position = False
+                self.current_holding_period = 0
+                self.capital += self.data.iloc[self.current_step]['close']
+        else:  # Hold
+            pass
 
     def step(self, action):
         self.current_step += 1
         self.current_holding_period += 1
-
-        if self.current_step >= len(self.data) - 1:
+        
+        if self.current_step >= len(self.data) - 1:  # Add capital check here
             done = True
         else:
             done = False
@@ -43,41 +59,29 @@ class StockTradingEnvironment(gym.Env):
             reward = self.data.iloc[self.current_step + 1]['close'] - self.data.iloc[self.current_step]['close']
         else:
             reward = 0
-
-        if action == 0:  # Buy
-            if not self.in_position:
-                self.in_position = True
-                self.current_holding_period = 0
-        elif action == 1:  # Sell
-            if self.in_position:
-                self.in_position = False
-                self.current_holding_period = 0
-        else:  # Hold
-            pass
-
-       # if self.in_position and self.current_holding_period >= self.max_holding_period:
-       #     self.in_position = False
-       #     self.current_holding_period = 0
-
+        
+        self.take_action(action)
+        
+        if self.in_position and self.current_holding_period >= self.max_holding_period:  
+            self.in_position = False
+            self.current_holding_period = 0
+            self.capital += self.data.iloc[self.current_step]['close']
+            
         next_state = self.get_state()
-
         return next_state, reward, done, {}
 
 def preprocess_data(data):
     data = data.copy()
-    """data['7-day'] = data['close'].rolling(window=7).mean()
-    data['14-day'] = data['close'].rolling(window=14).mean()
-    data['21-day'] = data['close'].rolling(window=21).mean()"""
-    
+       
     print(data.columns)
     
     data.dropna(inplace=True) 
     scaler = StandardScaler()
-    data[[ 'open','close', 'volume', 'updown', 'high', 'low','MACD', 'Signal', 'rsi', 'market_open','market_close', 'market_volume', 'market_updown', 'market_high', 'market_low','market_MACD', 'market_Signal', 'market_rsi']] = scaler.fit_transform(data[[  'open','close', 'volume', 'updown', 'high', 'low','MACD', 'Signal', 'rsi', 'market_open','market_close', 'market_volume', 'market_updown', 'market_high', 'market_low','market_MACD', 'market_Signal', 'market_rsi']])
+    data[['open','close', 'volume', 'high', 'low','MACD','Signal', 'rsi', 'market_updown']] = scaler.fit_transform(data[['open','close', 'volume', 'high', 'low','MACD','Signal', 'rsi', 'market_updown']])
     
     return data, scaler
 
-def q_learning(env, num_episodes=1500, alpha=0.1, gamma=0.99, epsilon_start=1.0, epsilon_end=0.02, epsilon_decay=0.999):
+def q_learning(env, num_episodes=6000, alpha=0.1, gamma=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.999 ):
     q_table = defaultdict(lambda: np.zeros(env.action_space.n))
 
     for episode in range(num_episodes):
@@ -163,10 +167,11 @@ def test_harness(historical_data, q_table, scaler, starting_capital=1000):
                 env.in_position = False
                 env.current_holding_period = 0
 
+        env.capital = capital
         state, _, done, _ = env.step(action)
         state = tuple(state)
 
-        if done:
+        if done or capital <= 0:
             break
 
     # Sell any remaining shares at the end of the simulation
@@ -203,25 +208,40 @@ def main():
     # Create the environment
     train_env = StockTradingEnvironment(train_data)
     test_env = StockTradingEnvironment(test_data)
-    
-    # Train the Q-learning model
-    q_table = q_learning(train_env, num_episodes=3000)
 
-    # Save the Q-table
-    q_table_file = 'q_table.pkl'
-    save_q_table(q_table, q_table_file)
+    # Define the range for epsilon decay
+    epsilon_decay_range = [0.7, 0.999]
 
-    # Load the Q-table
-    #q_table = load_q_table(q_table_file)
+    # Define the number of random search iterations
+    num_iterations = 100
+
+    # Keep track of the best decay rate and corresponding profit/loss
+    best_decay_rate = None
+    best_profit_or_loss = -float('inf')
+
+    for i in range(num_iterations):
+        # Randomly sample a decay rate from the range
+        decay_rate = random.uniform(epsilon_decay_range[0], epsilon_decay_range[1])
+
+        # Train the Q-learning model with the current decay rate
+        q_table = q_learning(train_env, num_episodes=6000, epsilon_decay=decay_rate)
+
+        # Evaluate the model using the test harness
+        profit_or_loss, _ = test_harness(test_data, q_table, scaler, starting_capital=10000)
+
+        print(f"Iteration {i+1}/{num_iterations}: Decay Rate = {decay_rate}, Profit/Loss = ${profit_or_loss:.2f}")
+
+        # Update the best decay rate and profit/loss if this is the best one so far
+        if profit_or_loss > best_profit_or_loss:
+            best_decay_rate = decay_rate
+            best_profit_or_loss = profit_or_loss
+
+    print(f"Best Decay Rate = {best_decay_rate}, Best Profit/Loss = ${best_profit_or_loss:.2f}")
 
     # Test the Q-learning model
-    recommendation = test_q_learning(q_table, test_env)   
-    print(f'Recommended action: {recommendation}')
+    # recommendation = test_q_learning(q_table, test_env)   
+    # print(f'Recommended action: {recommendation}')
     
-   # Test the Q-learning model using the test harness
-    profit_or_loss, actions_log = test_harness(test_data, q_table, scaler, starting_capital=1000)
-
-    print(f'Profit or Loss: ${profit_or_loss:.2f}')
 
 if __name__ == "__main__":
     main()
